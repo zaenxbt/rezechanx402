f1f8684c7e7c733cbf4a541118e425b099dd60f3
zaenxbt <zaenxbt@gmail.com>
2025-11-27 01:51:16 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,2 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {};
 module.exports = nextConfig;
-
-
-// postcss.config.js
-module.exports = {
-plugins: {
-tailwindcss: {},
-autoprefixer: {},
-},
-};
