81a02fa76f3ae5504379727c01ac63d57215baf8
zaenxbt <zaenxbt@gmail.com>
2025-11-27 01:45:42 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+module.exports = nextConfig;
+
+
+// postcss.config.js
+module.exports = {
+plugins: {
+tailwindcss: {},
+autoprefixer: {},
+},
+};
